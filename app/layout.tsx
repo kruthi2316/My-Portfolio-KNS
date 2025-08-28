8e00b2f130c5374a3fef71e224a7ca7dcd2ebbb5
Kruthi N <kruthi@Kruthis-MacBook-Air.local>
2025-08-28 11:59:59 -0400

    imported analytics  in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter, Montserrat, Quicksand } from "next/font/google"
 import "./globals.css"
-
+import { Analytics } from "@vercel/analytics/next"
 const quicksand = Quicksand({
   subsets: ["latin"],
   display: "swap",
